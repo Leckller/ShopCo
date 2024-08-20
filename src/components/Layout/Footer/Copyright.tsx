db6b17d2d5d76574790ecd77e31a8130ff83353b
Gusttavo Ruy <7leckler@gmail.com>
2024-08-20 09:53:17 -0300

    feat: parte do copyright concluida, responsividade feita tbm

--- src/components/Layout/Footer/Copyright.tsx
+++ src/components/Layout/Footer/Copyright.tsx
@@ -1,13 +1,26 @@
+import Visa from '../../../assets/Pay/Visa.svg';
+import PayPal from '../../../assets/Pay/Paypal.svg';
+import Mastercard from '../../../assets/Pay/Mastercard.svg';
+import GooglePay from '../../../assets/Pay/GooglePay.svg';
+import ApplePay from '../../../assets/Pay/ApplePay.svg';
+
 function Copyright() {
   return (
-    <section>
-      <small>Shop.co © 2000-2023, All Rights Reserved</small>
-      <article>
-        <img src="" alt="Visa" />
-        <img src="" alt="Elo" />
-        <img src="" alt="PayPal" />
-        <img src="" alt="ApplePay" />
-        <img src="" alt="GooglePay" />
+    <section
+      className="border-t-2 w-full flex flex-row
+       justify-center md:justify-between pt-2 pb-2 flex-wrap gap-5 md:gap-0
+      "
+    >
+      <small>
+        Shop.co © 2000-2023, All Rights Reserved
+      </small>
+      <article
+        className="flex flex-row flex-grow gap-5 justify-center
+       md:justify-end flex-wrap"
+      >
+        {[Visa, Mastercard, PayPal, ApplePay, GooglePay].map((pay) => (
+          <img src={ pay } key={ pay.toString() } alt={ pay.toString() } />
+        ))}
       </article>
     </section>
   );
