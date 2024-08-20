6a1861f0a10d2c901e01ffc4ea16593d5c45cda5
Gusttavo Ruy <7leckler@gmail.com>
2024-08-20 09:16:29 -0300

    feat: fiz o formulario do footer, já responsivo

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,10 +2,12 @@ import { Copyright, Form, Infos } from './Footer/index';
 
 function Footer() {
   return (
-    <footer>
-      <Form />
-      <Infos />
-      <Copyright />
+    <footer className="w-full flex justify-center items-center">
+      <div className="flex flex-col w-[90%] justify-between items-center gap-5 p-1">
+        <Form />
+        <Infos />
+        <Copyright />
+      </div>
     </footer>
   );
 }
