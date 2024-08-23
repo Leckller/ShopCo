24667024a0f336e843c3d2fdb23b85bc856f2f48
Gusttavo Ruy <7leckler@gmail.com>
2024-08-23 20:50:40 -0300

    feat: tentativa de fazer o carrossel p pc

--- src/components/Home/CarrosselRoupas.tsx
+++ src/components/Home/CarrosselRoupas.tsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import IRoupa from '../../Types/IRoupa';
 import Roupa from './Roupa';
 
 function CarrosselRoupas({ roupas, title }: { roupas: IRoupa[], title: string }) {
+  const [x, setX] = useState(0);
   return (
     <section className="w-[100%] items-center flex flex-col gap-10">
       <h1 className="text-3xl font-bold">{title}</h1>
-      <section
+      <button
         className="flex flex-col h-[425px] flex-wrap overflow-x-scroll
         snap-x carrossel gap-10"
+        // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+        onMouseOver={ (e) => setX(e.clientX) }
+        onMouseDown={ (e) => {
+          // e.target.scroll(x, 0);
+        } }
       >
         {roupas.map((roupa) => (
           <Roupa roupa={ roupa } key={ roupa.nome } />
         ))}
-      </section>
+      </button>
     </section>
   );
 }
--- src/components/Home/Roupa.tsx
+++ src/components/Home/Roupa.tsx
@@ -1,23 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import IRoupa from '../../Types/IRoupa';
 import Avaliacao from '../Gen/Avaliacao';
+import IRoupa from '../../Types/IRoupa';
 import Preco from '../Gen/Preco';
 
-function Roupa({ roupa }: { roupa:IRoupa }) {
+function Roupa({ roupa }: { roupa: IRoupa }) {
   const navigate = useNavigate();
   const { avaliacao, desconto, img, nome, preco, id } = roupa;
   return (
     <button
       className="snap-center flex flex-col gap-2 max-w-[400px] min-w-[200px] w-[20%]
-      flex-grow justify-between"
-      onClick={ () => navigate(`/ProductDetail/${id}`) }
+      justify-between flex-grow"
+      onClick={ () => {
+        navigate(`/ProductDetail/${id}`);
+        // eslint-disable-next-line no-restricted-globals
+        scroll(0, 0);
+      } }
     >
 
       <img className="rounded-3xl" src={ img } alt={ nome } />
-      <h2 className="text-2xl font-bold text-start">{nome}</h2>
 
-      <section className="flex flex-col gap-5">
+      <section className="flex flex-col flex-grow gap-2">
 
+        <h2 className="h-[100px] text-2xl font-bold text-start">{nome}</h2>
         <Avaliacao avaliacao={ avaliacao } />
         <Preco desconto={ desconto } preco={ preco } />
 
